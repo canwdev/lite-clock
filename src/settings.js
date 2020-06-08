2374cd343c2dd41c39d6ca2e0b62db885c4a1ace
canwdev <c2097@live.com>
2020-06-08 19:05:27 +0800

    bing wallpaper

--- src/settings.js
+++ src/settings.js
@@ -6,6 +6,11 @@ const toggleEl = document.getElementById('settings_toggle')
 const listEl = document.getElementById('settings_list')
 const queryObj = getQueryObj()
 
+/**
+ * 切换是否显示设置图标
+ * @returns {function(...[*]=)}
+ * @private
+ */
 function _toggleSettingsDisplay() {
   let flag = true
   return function () {
@@ -18,34 +23,60 @@ const toggleSettingsDisplay = _toggleSettingsDisplay()
 toggleEl.onclick = toggleSettingsDisplay
 listEl.style.display = 'none'
 
+
 /**
- * 切换主题
- * @returns {function(...[*]=)}
- * @private
+ * 切换Bing壁纸
  */
-function _toggleTheme() {
-  let dark = true
-  return function () {
-    if (dark) {
-      rootEl.classList.add('light-theme')
-    } else {
-      rootEl.classList.remove('light-theme')
-    }
-    updateQuery({
-      theme: dark ? 'light' : null
-    })
-    dark = !dark
+let flagThemeBing = false
+
+function toggleBing(disable) {
+  if (disable) flagThemeBing = false
+
+  if (!flagThemeBing) {
+    setBingWallpaper()
+    rootEl.classList.add('bing')
+    rootEl.classList.remove('light-theme')
+  } else {
+    setBingWallpaper(true)
+    rootEl.classList.remove('bing')
   }
+  updateQuery({
+    theme: flagThemeBing ? null : 'bing'
+  })
+  flagThemeBing = !flagThemeBing
 }
 
-const toggleTheme = _toggleTheme()
-if (queryObj.theme === 'light') {
+
+/**
+ * 切换主题
+ */
+let flagThemeDark = false
+
+function toggleTheme() {
+  toggleBing(true)
+  if (!flagThemeDark) {
+    rootEl.classList.add('light-theme')
+  } else {
+    rootEl.classList.remove('light-theme')
+  }
+  updateQuery({
+    theme: flagThemeDark ? null : 'light'
+  })
+  flagThemeDark = !flagThemeDark
+}
+
+if (queryObj.theme === 'bing') {
+  toggleBing()
+} else if (queryObj.theme === 'light') {
   toggleTheme()
 }
 
+/**
+ * 添加设置图标
+ */
 function addSettings() {
-
   const settingsList = [
+    {name: 'B', action: toggleBing},
     {name: '☯', action: toggleTheme},
     {
       name: '▢', action: () => {
@@ -66,3 +97,21 @@ function addSettings() {
 }
 
 addSettings()
+
+function setBingWallpaper(clear = false) {
+  if (clear) {
+    document.body.style.backgroundImage = null
+    return
+  }
+
+  // 有跨域
+  /*import('axios').then(({default: axios}) => {
+    axios.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1').then(res => {
+      console.log(res)
+    }).catch(e => {
+      console.error(e)
+    })
+  })*/
+
+  document.body.style.backgroundImage = `url('https://api.dujin.org/bing/1920.php')`
+}
