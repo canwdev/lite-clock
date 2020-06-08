107bfbbd063cee1994491bca0747f33a7d40f02e
canwdev <c2097@live.com>
2020-06-08 18:30:20 +0800

    保存主题设置到query

--- src/settings.js
+++ src/settings.js
@@ -1,39 +1,68 @@
 import screenfull from "screenfull"
+import {getQueryObj, updateQuery} from "./utils"
 
 const rootEl = document.getElementById('root')
 const toggleEl = document.getElementById('settings_toggle')
 const listEl = document.getElementById('settings_list')
+const queryObj = getQueryObj()
 
-function toggleSettingsDisplay() {
+function _toggleSettingsDisplay() {
   let flag = true
   return function () {
     flag = !flag
     listEl.style.display = flag ? 'none' : null
   }
 }
-const toggle = toggleSettingsDisplay()
-toggleEl.onclick = toggle
+
+const toggleSettingsDisplay = _toggleSettingsDisplay()
+toggleEl.onclick = toggleSettingsDisplay
 listEl.style.display = 'none'
+
+/**
+ * 切换主题
+ * @returns {function(...[*]=)}
+ * @private
+ */
+function _toggleTheme() {
+  let dark = true
+  return function () {
+    if (dark) {
+      rootEl.classList.add('light-theme')
+    } else {
+      rootEl.classList.remove('light-theme')
+    }
+    updateQuery({
+      theme: dark ? 'light' : null
+    })
+    dark = !dark
+  }
+}
+
+const toggleTheme = _toggleTheme()
+if (queryObj.theme === 'light') {
+  toggleTheme()
+}
 
 function addSettings() {
 
   const settingsList = [
-    {name: '▢', action: () => {screenfull.toggle()}},
-    {name: '☯', action: toggleTheme}
+    {name: '☯', action: toggleTheme},
+    {
+      name: '▢', action: () => {
+        screenfull.toggle()
+      }
+    },
   ]
 
   settingsList.forEach(item => {
     const btn = document.createElement('button')
     btn.onclick = () => {
       item.action()
-      toggle()
+      toggleSettingsDisplay()
     }
     btn.innerText = item.name
     listEl.appendChild(btn)
   })
 }
-addSettings()
 
-function toggleTheme() {
-  rootEl.classList.toggle('light-theme')
-}
+addSettings()
---
+++ src/utils/index.js
@@ -0,0 +1,29 @@
+import qs from "qs"
+
+export function getQueryObj() {
+  return qs.parse(location.search.slice(1))
+}
+
+/**
+ * 更新query
+ * @param obj
+ */
+export function updateQuery(obj) {
+  const query = getQueryObj()
+
+  for (let key in obj) {
+    if (obj[key] === null) {
+      delete query[key]
+      delete obj[key]
+    }
+  }
+
+  const queryString = qs.stringify({...query, ...obj})
+
+  if (history.pushState) {
+    const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + queryString
+    window.history.pushState({path: newurl}, '', newurl);
+  } else {
+    location.search = queryString
+  }
+}
