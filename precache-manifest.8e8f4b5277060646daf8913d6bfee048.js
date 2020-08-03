4a464c9b6b9be85e86376c53710d8ff62ce9ddcf
canwdev <c2097@live.com>
2020-08-03 12:15:40 +0000

    Deploying to gh-pages from  @ 4104289c682f49a191b82553335aebd5c342f50f 🚀

---
+++ precache-manifest.8e8f4b5277060646daf8913d6bfee048.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ee65d1547bfd9e86c6",
+    "url": "css/chunk-vendors.33eb119a.css"
+  },
+  {
+    "revision": "8b9658e99f2210a31090",
+    "url": "css/index.71f60418.css"
+  },
+  {
+    "revision": "b8365ca9bf80f0a9c1e439008d4887da",
+    "url": "data.json"
+  },
+  {
+    "revision": "5f02529c1aa0408b757189f2a5e26789",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "bc3d32a696895f78c19df6c717586a5d",
+    "url": "img/bg.bc3d32a6.svg"
+  },
+  {
+    "revision": "5d7b1d7c6e7f149292a624a24ec32a06",
+    "url": "index.html"
+  },
+  {
+    "revision": "b4ee65d1547bfd9e86c6",
+    "url": "js/chunk-vendors.ad79fe11.js"
+  },
+  {
+    "revision": "8b9658e99f2210a31090",
+    "url": "js/index.15eece79.js"
+  },
+  {
+    "revision": "ef8412dd127398ca1582ec9e5ce84b1f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,34 @@
-if(!self.define){const e=e=>{"require"!==e&&(e+=".js");let s=Promise.resolve();return r[e]||(s=new Promise(async s=>{if("document"in self){const r=document.createElement("script");r.src=e,document.head.appendChild(r),r.onload=s}else importScripts(e),s()})),s.then(()=>{if(!r[e])throw new Error(`Module ${e} didn’t register its module`);return r[e]})},s=(s,r)=>{Promise.all(s.map(e)).then(e=>r(1===e.length?e[0]:e))},r={require:Promise.resolve(s)};self.define=(s,c,i)=>{r[s]||(r[s]=Promise.resolve().then(()=>{let r={};const a={uri:location.origin+s.slice(1)};return Promise.all(c.map(s=>{switch(s){case"exports":return r;case"module":return a;default:return e(s)}})).then(e=>{const s=i(...e);return r.default||(r.default=s),r})}))}}define("./service-worker.js",["./workbox-468c4d03"],(function(e){"use strict";e.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"./favicon.png",revision:"728cabda15de8ca51c6d713a4a839c3b"},{url:"./index.html",revision:"c7182c7e245a14b76f0224dcb3d259e1"},{url:"./manifest.json",revision:"c7f815e96b2a37abbf9ad5a81ed88f73"},{url:"./static/css/main.4e6c46b043de889cb8a4.css",revision:"c1e0651311be55471e26e10953f8d711"},{url:"./static/js/0.baf74265c87b72bd9148.js",revision:"1b56b5ca42fda17ca289716b2747ddf2"},{url:"./static/js/0.baf74265c87b72bd9148.js.LICENSE.txt",revision:"af4701217085bd911d03758fa1b02cc0"},{url:"./static/js/1.a292a56e7f7a47196a91.js",revision:"077f32b04b90eb5122242644831a758c"},{url:"./static/js/1.a292a56e7f7a47196a91.js.LICENSE.txt",revision:"4e230d017ca1e1e1cdf42b987636e3ca"},{url:"./static/js/3.5c3af617ead8fc401461.js",revision:"2fb4f8cd9a8b391df2af9ce7c6bb73ec"},{url:"./static/js/runtime.c96ad1e23ad63da4d304.js",revision:"0358997880c41faa6ba2c6d73904d67c"}],{})}));
-//# sourceMappingURL=service-worker.js.map
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.8e8f4b5277060646daf8913d6bfee048.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "always-on-screen"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
